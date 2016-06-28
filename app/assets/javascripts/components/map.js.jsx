a375e3013cbdd738b70c199979a5fb526c3be6a4
nlarso <nlarso@gmail.com>
2016-06-27 21:48:24 -0600

    Improve KML filtering

--- app/assets/javascripts/components/map.js.jsx
+++ app/assets/javascripts/components/map.js.jsx
@@ -7,19 +7,25 @@ class Map extends React.Component {
       lat: 37.096528,
       lng: -113.568416,
       zoom: 13,
-      filterOptions: {
-        short: false,
-        medium: false,
-        long: false,
-        very_long: false,
-        easy: false,
-        moderate: false,
-        strenuous: false,
-        extreme: false,
-        spring: false,
-        summer: false,
-        fall: false,
-        winter: false,
+      filter: {
+        length: {
+          short: false,
+          medium: false,
+          long: false,
+          very_long: false,
+        },
+        difficulty: {
+          easy: false,
+          moderate: false,
+          strenuous: false,
+          extreme: false,
+        },
+        season: {
+          spring: false,
+          summer: false,
+          fall: false,
+          winter: false,
+        }
       }
     }
   }
@@ -62,35 +68,53 @@ class Map extends React.Component {
     return new google.maps.KmlLayer(url, kmlOptions);
   }
 
-  filterKmlLayers() {
-    const { filterOptions, kmlFiles } = this.state;
-    kmlFiles.map((kmlFile) => {
-      let { kmlLayer } = kmlFile
-      if (filterOptions[kmlFile.length] || filterOptions[kmlFile.difficulty] || filterOptions[kmlFile.season]) {
-        if (kmlLayer.map == null) {
-          kmlLayer.setMap(this.map)
-        }
-      } else {
-        if (kmlLayer.map != null) {
-          kmlLayer.setMap(null)
-        }
-      }
+  allFalse(category = null) {
+    const { filter } = this.state;
+    const values = category ? _.values(filter[category]) :  _.flatten(_.map(filter, _.values));
+
+    return values.every((item) => {
+      return !item;
     });
   }
 
-  onInputChange(event) {
-    const { name, checked } = event.target;
-    const { filterOptions } = this.state;
-    filterOptions[name] = checked
-    this.setState({filterOptions})
+  filterKmlLayers() {
+    const { filter, kmlFiles } = this.state;
+    if (!this.allFalse()) {
+      kmlFiles.map((kmlFile) => {
+        let { kmlLayer } = kmlFile;
+        const lengthFilter = filter['length'][kmlFile.length] || this.allFalse('length');
+        const difficultyFilter = filter['difficulty'][kmlFile.difficulty] || this.allFalse('difficulty');
+        const seasonFilter = filter['season'][kmlFile.season] || this.allFalse('season');
+        if (lengthFilter && difficultyFilter && seasonFilter) {
+          if (kmlLayer.map == null) {
+            kmlLayer.setMap(this.map)
+          }
+        } else if (kmlLayer.map != null) {
+          kmlLayer.setMap(null)
+        }
+      });
+    }
   }
 
-  renderCheckbox(name, label = null) {
+  onInputChange(kind, event) {
+    const { name, checked } = event.target;
+    const { filter } = _.clone(this.state);
+    filter[kind][name] = checked
+    this.setState({filter})
+  }
+
+  renderCheckbox(options = {}) {
+    const { category, name, label } = options;
     return (
       <div className="form-group no-margin">
         <div className="checkbox">
           <label>
-            <input type="checkbox" name={name} onChange={this.onInputChange} checked={this.state.filterOptions[name]} />
+            <input
+              checked={this.state.filter[name]}
+              name={name}
+              onChange={this.onInputChange.bind(this, category)}
+              type="checkbox"
+            />
             {label || _.capitalize(name)}
           </label>
         </div>
@@ -99,30 +123,29 @@ class Map extends React.Component {
   }
 
   render () {
-    const { filterOptions } = this.state;
     return (
       <div className="row">
         <div className="col-sm-2">
           <div className="checkbox-group">
             <h5>Length</h5>
-            {this.renderCheckbox("short", "1-2 Hours")}
-            {this.renderCheckbox("medium", "4-6 Hours")}
-            {this.renderCheckbox("long", "6-9 Hours")}
-            {this.renderCheckbox("very_long", "10-12 Hours")}
+            {this.renderCheckbox({name: "short", category: "length", label: "1-2 Hours"})}
+            {this.renderCheckbox({name: "medium", category: "length", label: "4-6 Hours"})}
+            {this.renderCheckbox({name: "long", category: "length", label: "6-9 Hours"})}
+            {this.renderCheckbox({name: "very_long", category: "length", label: "10-12 Hours"})}
           </div>
           <div className="checkbox-group">
             <h5>Difficulty</h5>
-            {this.renderCheckbox("easy", "Family (Easy)")}
-            {this.renderCheckbox("moderate")}
-            {this.renderCheckbox("strenuous")}
-            {this.renderCheckbox("extreme")}
+            {this.renderCheckbox({name: "easy", category: "difficulty", label: "Family (Easy)"})}
+            {this.renderCheckbox({name: "moderate", category: "difficulty"})}
+            {this.renderCheckbox({name: "strenuous", category: "difficulty"})}
+            {this.renderCheckbox({name: "extreme", category: "difficulty"})}
           </div>
           <div className="checkbox-group">
             <h5>Season</h5>
-            {this.renderCheckbox("spring")}
-            {this.renderCheckbox("summer")}
-            {this.renderCheckbox("fall")}
-            {this.renderCheckbox("winter")}
+            {this.renderCheckbox({name: "spring", category: "season"})}
+            {this.renderCheckbox({name: "summer", category: "season"})}
+            {this.renderCheckbox({name: "fall", category: "season"})}
+            {this.renderCheckbox({name: "winter", category: "season"})}
           </div>
         </div>
         <div className="col-sm-10">
